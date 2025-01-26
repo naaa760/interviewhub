cd916355d4828e0a7ae58a0b0f4317ed992e0fbd
nehhhhha6 <n0370525@gmail.com>
2025-01-26 12:10:44 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-// import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div className="m-10">Home</div>;
+  return (
+    <div className="m-10">
+      <SignInButton>
+        <Button>login</Button>
+      </SignInButton>
+    </div>
+  );
 }
